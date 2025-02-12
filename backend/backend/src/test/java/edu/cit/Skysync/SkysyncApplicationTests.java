f7c52a3f4cc99be3ec237a70951e73113e372da1
LAB14_USER ON WS15 <L14X08W15@citulab.net>
2025-02-12 09:46:30 +0800

    added backend

---
+++ backend/backend/src/test/java/edu/cit/Skysync/SkysyncApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.Skysync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkysyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
