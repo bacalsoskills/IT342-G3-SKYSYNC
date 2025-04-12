daaae560742b0f0ee7c619f8ce78518ddd9821ff
B4rryAli <christianbarryalico@gmail.com>
2025-04-13 03:17:04 +0800

    Backend Updates

--- backend/src/main/java/edu/cit/Skysync/entity/ActivityEntity.java
+++ backend/src/main/java/edu/cit/Skysync/entity/ActivityEntity.java
@@ -1,5 +1,8 @@
 package edu.cit.Skysync.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,9 +32,11 @@ public class ActivityEntity {
     private String weatherCondition;
     
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
-    
+
+    @OneToMany(mappedBy = "activity", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ScheduleEntity> schedules;
 
     public ActivityEntity(String name, String description, String weatherCondition) {
         this.name = name;
