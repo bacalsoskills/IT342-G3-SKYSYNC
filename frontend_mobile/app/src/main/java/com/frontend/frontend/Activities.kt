f4f0ce2bc9f8da3a3dacfa67c7bd6442f586c910
bacalsoskills <bacalsomichaelferdinand@gmail.com>
2025-04-15 20:39:46 +0800

    update
    update

--- frontend_mobile/app/src/main/java/com/frontend/frontend/Activities.kt
+++ frontend_mobile/app/src/main/java/com/frontend/frontend/Activities.kt
@@ -3,28 +3,25 @@ package com.frontend.frontend
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-//import androidx.activity.enableEdgeToEdge
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.frontend.R
 
-class HomePage : AppCompatActivity() {
-     override fun onCreate(savedInstanceState: Bundle?) {
+class Activities : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //enableEdgeToEdge()
-
-        setContentView(R.layout.activity_home_page)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_activities)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-         val startNowImageView = findViewById<ImageView>(R.id.imageView5)
-         startNowImageView.setOnClickListener {
-             val intent = Intent(this, LoginActivity::class.java)
-             startActivity(intent)
-         }
+
+
+
+
     }
 }
